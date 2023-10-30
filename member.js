d18d28fcdb6e8b8d664f7909374e6925a6ffb2ec
ToshihiroYamashita <46772133+ToshihiroYamashita@users.noreply.github.com>
2023-10-30 14:52:09 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,12 @@
+function skillsMember() {
+    return {
+        restrict: 'E',
+        templateUrl: 'modules/skills/viewsmember.html',
+        controller: 'skillsMemberController',
+        controllerAs: 'vm'
+        bindToController: 'true',
+        scope: {
+                member: 'm'
+        }
+    };
+}
